a5ce80c1bd11392e4c53b9e6fa3549228470e914
windy <707298413@qq.com>
2018-10-24 03:38:54 +0800

    修改示例

--- README.md
+++ README.md
@@ -2,11 +2,11 @@
 ```
 	1. only for linux 
 	2. 从源码安装lua5.3	
-	3. 
+	3. git clone https://github.com/HYbutterfly/scorpio.git
 	4. cd scorpio
 	5. make
 	6. ./scorpio main.lua
-	7. 在另一个命令窗口 用 nc 127.0.0.1 8888 连接服务器 并输入一些内容 (发生 bye 服务器将关闭)
+	7. 在另一个命令窗口 用 nc 127.0.0.1 8888 连接服务器 并输入一些内容 (发送 bye 服务器将关闭)
 ```
 
 
--- main.lua
+++ main.lua
@@ -5,16 +5,16 @@ local socket = require "scorpio.socket"
 
 local server = scorpio.server("127.0.0.1", 8888)
 
+
 server:on("connection", function (sock)
 
 	print("connect from " .. sock.addr .. " " .. sock.fd)
 
 	sock:on("message", function ( str )
-		print("recv:", sock.fd, str:sub(1, #str-1))
+		print("recv:", str:sub(1, #str-1))
+		sock:send(str)
 		if str == "bye\n" then
 			scorpio.exit()
-		else
-			sock:send(str)
 		end
 	end)
 
@@ -25,30 +25,15 @@ server:on("connection", function (sock)
 end)
 
 
--- 设置主循环的间隔时间 
-scorpio.hint("delay", 100)
-
---[[
--- will to do
-scorpio.hint("me", "game1")
-scorpio.hint("cluster", {
-		{ name = 'hall1', 	ip = '127.7.0.1', port = 8000 },
-		{ name = 'hall2', 	ip = '127.7.0.1', port = 8000 },
-		{ name = 'game1',   ip = '127.7.0.1', port = 8001 },
-		{ name = 'game2',   ip = '127.7.0.1', port = 8001 },
-		{ name = 'mongo',  	ip = '127.7.0.1', port = 8002 },
-	})
---]]
-
 scorpio.start(function ( )
-	print("scorpio start ...")
+	print("Listen on 127.0.0.1:8888")
 	
 	timer.create(3000, function (count)
 		print("timer1 timeout", count)
 	end, -1)
 
 	timer.create(1000, function ()
-		print("timer2: i will been do only once")
+		print("timer2: i only say once")
 	end)
 end)
 
